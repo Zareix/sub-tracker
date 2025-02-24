4bbdf5d4f3cbf66b6699e6ec496045867c5b3000
Raphaël C. <raphcatarino@gmail.com>
2025-02-24 19:14:50 +0100

    sidebar start

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { AppSidebar } from "~/components/ui/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,11 +9,13 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
         <title>Sub Tracker</title>
         <meta name="description" content="Track your subscriptions" />
       </Head>
-      <div>
-        <main className="container mx-auto grid min-h-screen px-4 pt-8 xl:max-w-5xl">
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="container mx-auto min-h-screen px-4 pt-8 xl:max-w-5xl">
+          <SidebarTrigger />
           {children}
         </main>
-      </div>
+      </SidebarProvider>
     </>
   );
 };
