9ec75b58e3b5014a0a72af505ed5a2d5702645a1
Raphaël Catarino <raphcatarino@gmail.com>
2025-02-25 20:19:39 +0100

    fix

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,8 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./server/db/migrate");
-    await (await import("./server/exchange-rates")).updateExchangeRates();
+    if (process.env.NODE_ENV !== "development") {
+      await (await import("./server/exchange-rates")).updateExchangeRates();
+    }
   }
 }
