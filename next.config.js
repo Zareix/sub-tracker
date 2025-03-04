18e532e907f55e01938bc413ad02dc4770c446aa
Raphaël Catarino <raphcatarino@gmail.com>
2025-03-04 18:19:52 +0100

    fix next

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  ...withBundleAnalyzer(),
   reactStrictMode: true,
 
   images: {
@@ -19,11 +14,6 @@ const config = {
     ],
   },
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
