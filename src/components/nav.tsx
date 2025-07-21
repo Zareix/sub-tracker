36ea5b0d0414087f9eef09ea72ba02836a843bfb
Raphaël Catarino <raphcatarino@gmail.com>
2025-07-21 18:22:28 +0200

    fix: lint

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,9 +9,9 @@ import {
 	UserCircle2Icon,
 	WrenchIcon,
 } from "lucide-react";
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
 import { CreateSubscriptionDialog } from "~/components/subscriptions/create";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
